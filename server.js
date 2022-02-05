ce4e3d7ad06028ba1c65f2020847da655970aac2
will tong <46694175+willtong77@users.noreply.github.com>
2022-02-05 18:49:52 -0500

    added api stuff

---
+++ server.js
@@ -0,0 +1,94 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+require('dotenv').config();
+
+// Setting up Mongoose
+const Schema = mongoose.Schema;
+
+const UrlSchema = new Schema({
+  longUrl: { type: String, required: true},
+  shortId: { type: String, required: true},
+})
+
+const Url = mongoose.model('Url', UrlSchema);
+
+// Setting up Database
+const password = process.env.DB_ADMIN_PASSWORD;
+const dbname = process.env.DB_USERNAME;
+const URI = `mongodb+srv://will:${password}@pocketurl-api.qyulk.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+mongoose.connect(URI, option);
+
+mongoose.connection.on('open', () => {
+  console.log('Connected to MongoDB!');
+  
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
+
+// Routes
+
+// GET
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/urls', async (req, res) => {
+  const shortId = req.query.shortId;
+  if (!shortId) {
+    res
+      .status(400)
+      .json({
+        message:'You must query the database based on a shortUrl.'
+      })
+  } else {
+    try {
+      const data = await Url.findOne({ shortId: shortId });
+      res.json({ data: data });
+    } catch (err) {
+      res.json({ data: null });
+      console.log(err);
+    }
+  }
+});
+
+// GET REDIRECT
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const url = await Url.findOne({ shortId: shortId });
+  try {
+    res.redirect(url.longUrl);
+  } catch (err) { console.log(err); }
+})
+
+// POST
+app.post('/urls/:short/', async function(req, res) {
+  const { long } = req.body;
+  if (!req.params.short || !long) {
+    res.status(800).json({ message: "need long and short man!!!" })
+    return;
+  }
+  
+  const data = await Url.create({ longUrl: long, shortId: req.params.short });
+  res.status(201).json({ data });
+});
