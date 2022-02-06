e807de25accba6a3633bcdabdded4315217c4ee4
will tong <46694175+willtong77@users.noreply.github.com>
2022-02-05 19:56:08 -0500

    .

--- server.js
+++ server.js
@@ -70,11 +70,6 @@ app.get('/urls', async (req, res) => {
   }
 });
 
-// BLANK
-app.get('/', (req, res) => {
-  res.json({ data: null });
-});
-
 // GET REDIRECT
 app.get('/:shortId', async (req, res) => {
   const shortId = req.params.shortId;
